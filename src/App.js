ed38ae69ab7cb864456652919a4a89fc203179c7
Nathan Chesmar <nathan.chesmar@gmail.com>
2022-05-27 13:43:12 -0700

    update file structure for screens

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import "firebase/compat/firestore";
 import { SignInForm } from "./screens/SignIn/SignInForm";
 import { getAuth } from "firebase/auth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Piece from "./screens/Piece";
+import UserProfile from "./screens/UserProfile";
 
 function App() {
   const variable = "this is props";
@@ -11,6 +15,13 @@ function App() {
       <header className="App-header">
         <SignInForm />
       </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home data={variable} />} />
+          <Route path="/user" element={<UserProfile data={variable} />} />
+          <Route path="/user/:pieceName" element={<Piece data={variable} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
